0f04ee174246f86d3fb19f346160a06b3f8f96d0
sbvgit <sbv5dn@virginia.edu>
2023-11-03 19:53:21 -0400

    updating m09 files

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='M09-hw',
-    version='v1.0.0',
+    version='v1.0.1',
     description='Swaroop Veerabhadrappa M09 HW',
     url='https://github.com/sbvgit/M09-hw',
     author='Swaroop Veerabhadrappa',
     author_email='sbv5dn@virginia.edu',
     license='MIT',
-    packages=['booklover.py']
+    packages=find_packages(['booklover.py'])
 )
